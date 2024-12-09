ba7e3751d1e9e21d5390ddfcea0e986e41a3e74e
jayarajcl <125452567+jayarajcl@users.noreply.github.com>
2024-12-09 16:29:30 +0530

    Update SQLInjection.java

--- Secure Compass- Testcases/src/SQLInjection.java
+++ Secure Compass- Testcases/src/SQLInjection.java
@@ -1,7 +1,21 @@
-public class SQLInjection {
-    public void doPost(HttpServletRequest request) {
-        String userInput = request.getParameter("userInput"); // Assume this comes from user input
-        String query = "SELECT * FROM users WHERE username = '" + userInput + "'";
-        System.out.println("Executing query: " + query);
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import java.io.StringReader;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.InputSource;
+
+public class XMLParser {
+    public static void main(String[] args) throws Exception {
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+                   + "<!DOCTYPE root [<!ENTITY ext SYSTEM \"file:///etc/passwd\">]>"
+                   + "<root>&ext;</root>";
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new InputSource(new StringReader(xml)));
+
+        System.out.println(document.getDocumentElement().getTextContent());
     }
 }
+
